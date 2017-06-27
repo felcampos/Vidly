829886c64ee170295f28d9d12270265a349773d9
Felipe Souza Campos <felipe.vra@gmail.com>
2017-06-27 17:19:53 -0300

    Commit With Movie Controller-View-Model

---
+++ Vidly/Controllers/MoveisController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoveisController : Controller
+    {
+        // GET: Moveis/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            return View(movie);
+        }
+    }
+}
